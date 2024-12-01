1061cb90bfee28d6db12f2d2379428a7706b9973
DharsDev <dharshanpalani.2006@gmail.com>
2024-12-01 17:23:09 +0530

    Changed style for text bubble (change 1)

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -86,7 +86,7 @@ function Chat({ socket, username, room }) {
                     username === messageContent.author
                       ? "rounded-br-none"
                       : "rounded-bl-none"
-                  } max-w-[100%]`}
+                  } max-w-[98%]`}
                 >
                   <div className='text-sm text-white leading-relaxed mb-2'>
                     {messageContent.message}
